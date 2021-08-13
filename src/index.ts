5e36b04abfc4776ea0006b670298d2b531664a2b
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-08-14 01:17:38 +0200

    Remove unnecessary exports and try to interopt with node modules
    Also change the version number to something below 1 since this is only a
    prototype

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-export const Greeter = (name: string): string => `Hello ${name}`
-
 import { beatmap } from "./beatmap/beatmap"
 import { oauth } from "./oauth/oauth"
 
-export default {
+const osuApiV2 = {
     beatmap,
     oauth,
 }
+
+// Typescript default export (import osuApiV2 from "osuApiV2")
+// NodeJs: (const osuApiV2 = require("osuApiV2").default)
+export default osuApiV2
+// This is not working:
+//// NodeJs compatibility (const osuApiV2 = require("osuApiV2"))
+//module.exports = osuApiV2
