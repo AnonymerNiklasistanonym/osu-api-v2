a0192e664c05f2ddb214603f9821afc8526f14b3
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-08-15 02:01:49 +0200

    Change osu-api-v2 test section name

--- test/osu_api_v2.test.ts
+++ test/osu_api_v2.test.ts
@@ -2,6 +2,6 @@ import { describe } from "mocha"
 
 import { endpointsTestSuite } from "./osu_api_v2/endpoints.test"
 
-describe("osu-api-v2-requests", () => {
+describe("osu-api-v2", () => {
     endpointsTestSuite()
 })
