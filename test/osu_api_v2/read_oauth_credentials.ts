646d0eadb7b7a80c221d3bd7b52034aa5bd600d5
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-08-23 20:40:18 +0200

    Try running osu api tests via GitHub Actions

--- test/osu_api_v2/read_oauth_credentials.ts
+++ test/osu_api_v2/read_oauth_credentials.ts
@@ -21,6 +21,18 @@ export const defaultOauthCredentialsFilePath = path.join(
 export const readOauthCredentials = async (
     filePath: string = defaultOauthCredentialsFilePath,
 ): Promise<OAuthCredentials> => {
+    // If variables are found use their values instead
+    if (
+        process.env.OSU_OAUTH_CLIENT_ID !== undefined &&
+        !isNaN(Number(process.env.OSU_OAUTH_CLIENT_ID)) &&
+        process.env.OSU_OAUTH_CLIENT_SECRET !== undefined &&
+        process.env.OSU_OAUTH_CLIENT_SECRET.length > 0
+    ) {
+        return {
+            clientId: Number(process.env.OSU_OAUTH_CLIENT_ID),
+            clientSecret: process.env.OSU_OAUTH_CLIENT_SECRET,
+        }
+    }
     const oauthCredentialsFileContent = await fsp.readFile(filePath, {
         encoding: "utf8",
     })
