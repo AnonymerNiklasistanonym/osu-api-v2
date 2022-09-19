044284ddd80357d2b1b2b2c5c7c318fec2d79294
hughp135 <hughp135@gh>
2022-09-19 19:08:50 +0100

    Add authorization code flow

---
+++ src/oauth/authorization_code_grant.ts
@@ -0,0 +1,51 @@
+import type { OauthAccessTokenWithRefresh } from "../types/oauth_access_token"
+import type { Fetch } from "../types/fetch"
+
+import { baseUrl } from "../types/api_info"
+import { OsuApiV2WebRequestError } from "../helpers/custom_errors"
+import { AuthorizationCodeGrant } from "../types/authorization_code_grant"
+
+declare const fetch: Fetch
+
+export const authorizationCodeGrant = async (
+    client_id: number,
+    client_secret: string,
+    redirect_uri: string,
+    code: string,
+): Promise<OauthAccessTokenWithRefresh> => {
+    const requestBody: AuthorizationCodeGrant = {
+        client_id,
+        client_secret,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri,
+    }
+    const method = "post"
+    const headers = { "Content-Type": "application/json" }
+    const body = JSON.stringify(requestBody)
+    // eslint-disable-next-line no-useless-catch
+    try {
+        const res = await fetch(`${baseUrl}/oauth/token`, {
+            body,
+            headers,
+            method,
+        })
+        if (res.status !== 200) {
+            throw new OsuApiV2WebRequestError(
+                `Bad web request (${res.status}=${res.statusText}, url=${res.url})`,
+                res.status,
+                res.statusText,
+                res.url,
+                method,
+                headers,
+                body,
+            )
+        }
+
+        const oauthAccessToken =
+            (await res.json()) as OauthAccessTokenWithRefresh
+        return oauthAccessToken
+    } catch (err) {
+        throw err
+    }
+}
