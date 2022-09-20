da9cc7a8c130ea1fd981c8f8604ec119bde15c13
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2022-09-20 23:55:10 +0200

    Update the documentation for the authorize redirect URL generator

--- src/oauth/authorize.ts
+++ src/oauth/authorize.ts
@@ -9,17 +9,28 @@ import { urlParameterGenerator } from "../helpers/url_parameter_generator"
  * permission from the user, they should be redirected to an authorize URL
  * that is created with this method.
  *
- * ([Source](https://osu.ppy.sh/docs/index.html#authorization-code-grant))
+ * If the user accepts your request, they will be redirected back to your site
+ * with a temporary single-use code contained in the URL parameter.
+ * If a state value was provided in the previous request, it will be returned
+ * here.
  *
- * @param clientId
- * @param redirectUri
- * @param scopes
- * @param state
+ * @param clientId The Client ID you received when you
+ * [registered](https://osu.ppy.sh/home/account/edit#new-oauth-application).
+ * @param redirectUri The URL in your application where users will be sent after
+ * authorization. This must match the registered Application Callback URL
+ * exactly.
+ * @param scopes A list of scopes.
+ * @param state Data that will be returned when a temporary code is issued.
+ * It can be used to provide a token for protecting against cross-site request
+ * forgery attacks.
+ * @returns The authorize redirect URL which opens the authorization dialogue.
+ *
+ * ([Source](https://osu.ppy.sh/docs/index.html#authorization-code-grant))
  */
 export const authorizeRedirectUrlGenerator = (
     clientId: number,
     redirectUri: string,
-    scopes: OsuApiV2AuthorizeScopes[],
+    scopes?: OsuApiV2AuthorizeScopes[],
     state?: string,
 ): string => {
     const params = urlParameterGenerator([
